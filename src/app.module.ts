8e06c5a79cd6ae18ca28440fdaca179db77cba29
Aagripa <fadhilraihank@gmail.com>
2023-11-03 16:13:56 +0700

    interceptor logging

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
-import { Module } from '@nestjs/common';
-import { DatabaseModule } from './core/database/Database.module';
+import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
+import { LoggingInterceptor } from './core/modules/interceptors/logging.interceptor'; // Pastikan Anda mengimpor LoggingInterceptor dari lokasi yang benar
+import { DatabaseModule } from './core/database/Database.module';
 import { AuthModule } from './core/modules/auth/auth.module';
 import { UsersModule } from './core/modules/users/users.module';
 import { ProjectModule } from './core/modules/project/project.module';
@@ -21,6 +23,17 @@ import { MailModule } from './core/modules/mail/mail.module';
     TaskModule,
     MailModule
   ],
-  
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // Implementasikan konfigurasi middleware di sini jika diperlukan
+    // Contoh:
+    // consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
